52422f35a1e0f790656e1996bd67832ecc35ae0f
yamy466 <almog.elisha@gmail.com>
2024-07-20 14:23:03 +0300

    sort

--- src/shared/components/users-table/users-table.component.ts
+++ src/shared/components/users-table/users-table.component.ts
@@ -1,16 +1,29 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
+import { ChangeDetectionStrategy, Component, input, output, signal, ViewChild, WritableSignal } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { User } from '../../models/user.model';
+import { NgFor, NgIf } from '@angular/common';
+import { MatSort, MatSortModule } from '@angular/material/sort';
 
 @Component({
   selector: 'app-users-table',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [MatTableModule, NgFor, NgIf, MatSortModule],
   templateUrl: './users-table.component.html',
   styleUrl: './users-table.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UsersTableComponent {
   users = input.required<User[]>();
-  // readonly displayedColumns = ;
+  userDblClicked = output<User>();
+  readonly displayedColumns: (keyof User)[] = ['name', 'phone', 'email', 'website'];
+  @ViewChild(MatSort) sort: MatSort;
+  dataSource: WritableSignal<MatTableDataSource<User>> = signal(new MatTableDataSource([]));
+
+  ngOnInit(): void {
+    this.dataSource().data = this.users();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource().sort = this.sort;
+  }
 }
